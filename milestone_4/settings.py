8d082ad5a4da0d8fd2e39b57cf505b4a2033e96a
brunobdias <devbrunobdias@gmail.com>
2021-10-26 22:12:22 +0000

    deployment

--- milestone_4/settings.py
+++ milestone_4/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-lvz+*=1&sx1!-#m$$-4=wci9v8!d2v$o0e)2h$45rulh^57jr9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['food-store-place.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -119,16 +119,18 @@ WSGI_APPLICATION = 'milestone_4.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': BASE_DIR / 'db.sqlite3',
-#    }
-#}
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
-DATABASES = {
-    'default': dj_database_url.parse("postgres://ajataccdqunyvt:788db5ddcc932acedc9aa66b04f457df5557b04f9b4ca49276439e70bc92fc10@ec2-79-125-30-28.eu-west-1.compute.amazonaws.com:5432/d5gks2282jhs9o")
-}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
